f3624032a295ab3b6c674347834c1794231cfff0
anaclaraetec <anaclaraetec2@gmail.com>
2025-11-25 20:31:16 -0300

    adicionado projeto inteiro

---
+++ app/src/main/java/com/example/myapplication/Alunos.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import java.io.Serializable;
+
+public class Alunos implements Serializable {
+    private int id;
+    private String nome;
+    private String documento;
+    private String curso;
+
+    public Alunos(int id, String name, String
+                 doc, String course){
+        this.id = id;
+        this.nome = name;
+        this.documento = doc;
+        this.curso = course;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getDocumento(){
+        return documento;
+    }
+    public String getCurso(){
+        return curso;
+    }
+
+
+}
