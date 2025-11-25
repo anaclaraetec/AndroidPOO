e24f7177b00ee74100081081d4ea8812946dd969
anaclaraetec <anaclaraetec2@gmail.com>
2025-11-25 20:48:30 -0300

    adicionado projeto inteiro

--- app/src/main/java/com/example/myapplication/LogicaCadastro.java
+++ app/src/main/java/com/example/myapplication/LogicaCadastro.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,7 +43,8 @@ public class LogicaCadastro extends AppCompatActivity {
      botao_cadastrar.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-             
+             Intent mudar_tela = new Intent();
+             startActivity(mudar_tela);
          }
      });
 
